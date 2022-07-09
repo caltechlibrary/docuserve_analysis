87eac32b51cecba480f3faa42e629c4ef421349b
dtwrublewski <dtwrublewski@NOTEBOOK-DW.local>
2022-07-08 20:26:49 -0700

    analysis script update

--- docuserve_analysis_script.R
+++ docuserve_analysis_script.R
@@ -5,25 +5,45 @@ install.packages("readxl")
 library("readxl")
 library("tidyverse")
 # place excel files for analysis in the /data/ folder
+
 # Imports XLSX file into dataframe:
 docuserve_data_all <- read_xlsx("./data/2021_docuserve_borrowing_requests.xlsx")
+
 # Filters for the selected columns:
 docuserve_data_filtered <- select(docuserve_data_all, `Request Type`, 
 `Loan Author`, `Loan Title`, `Loan Publisher`, `Loan Date`, `Loan Edition`, 
 `Photo Journal Title`, `Photo Journal Year`, `Transaction Date`, `Photo Item Author`, 
 `Photo Item Place`, `Photo Item Publisher`, `Photo Item Edition`, `Document Type`, 
 `Web Request Form`, `User Name1`, `Status`, `Department`)
+
 # Data frame for loans (books?):
 docuserve_loans <- docuserve_data_filtered %>%
   filter(`Request Type` == "Loan")
+
 # Data frame for articles:
 docuserve_articles <- docuserve_data_filtered %>%
   filter(`Request Type` == "Article")
-# Journal titles:
+
+# standardize capitalization - make all lowercase
+docuserve_articles$`Photo Journal Title` <- 
+  tolower(docuserve_articles$`Photo Journal Title`)
+
+# need regex to get rid of training period or comma
+docuserve_articles$`Photo Journal Title` <- 
+  gsub('^\\.|\\.$', '', docuserve_articles$`Photo Journal Title`)
+
+# get rid of trailing forward slash
+docuserve_articles$`Photo Journal Title` <- 
+  gsub(' /$', '', docuserve_articles$`Photo Journal Title`)
+
+#get rid of "the " in front of titles
+docuserve_articles$`Photo Journal Title` <- 
+  gsub('the ', '', docuserve_articles$`Photo Journal Title`)
+
+# extract Journal titles:
 journal_count <- docuserve_articles %>%
   count(`Photo Journal Title`)
+
 # alphabetical order
 journal_abc <- journal_count[order(journal_count$`Photo Journal Title`),]
-# need regex to get rid of training period or comma
-# need regex to standardize capitalization
-# need regex to get rid of "The " at the start of journals
+
