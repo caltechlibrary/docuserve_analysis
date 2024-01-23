f3d9e3809c46af49201e360835564ad12f025135
Donna Wrublewski <dtwrub@caltech.edu>
2024-01-23 14:44:44 -0800

    Update docuserve_analysis_script.R
    added more commands for cleanup (journal, trans, etc.)

--- docuserve_analysis_script.R
+++ docuserve_analysis_script.R
@@ -7,7 +7,7 @@ library("tidyverse")
 # place excel files for analysis in the /data/ folder
 
 # Imports XLSX file into dataframe:
-docuserve_data_all <- read_xlsx("./data/2021_docuserve_doc_del_requests.xlsx")
+docuserve_data_all <- read_xlsx("./data/2023_docuserve_doc_del_requests.xlsx")
 
 # Filters for the selected columns:
 docuserve_data_filtered <- select(docuserve_data_all, `Request Type`, 
@@ -36,6 +36,10 @@ docuserve_articles$`Photo Journal Title` <-
 docuserve_articles$`Photo Journal Title` <- 
   gsub(pattern = "\\,", '', docuserve_articles$`Photo Journal Title`)
 
+# remove all periods
+docuserve_articles$`Photo Journal Title` <- 
+  gsub(pattern = "\\.", '', docuserve_articles$`Photo Journal Title`)
+
 # get rid of trailing forward slash
 docuserve_articles$`Photo Journal Title` <- 
   gsub(' /$', '', docuserve_articles$`Photo Journal Title`)
@@ -48,6 +52,18 @@ docuserve_articles$`Photo Journal Title` <-
 docuserve_articles$`Photo Journal Title` <- 
   gsub('ü', 'u', docuserve_articles$`Photo Journal Title`)
 
+# replace ampersand with "and"
+docuserve_articles$`Photo Journal Title` <- 
+  gsub('&', 'and', docuserve_articles$`Photo Journal Title`)
+
+# replace "j" with "journal"
+docuserve_articles$`Photo Journal Title` <- 
+  gsub("(?i)\\b[j]\\b", "journal", docuserve_articles$`Photo Journal Title`)
+
+# replace "trans" with "transactions"
+docuserve_articles$`Photo Journal Title` <- 
+  gsub("trans", "transactions", docuserve_articles$`Photo Journal Title`)
+
 # extract Journal titles and arrange by number:
 journal_count <- docuserve_articles %>%
   count(`Photo Journal Title`, sort = TRUE)
@@ -56,7 +72,7 @@ journal_count <- docuserve_articles %>%
 journal_abc <- journal_count[order(journal_count$`Photo Journal Title`),]
 
 # export to csv files
-write_csv(journal_abc, file = "./data/2021_docuserve_doc_del_journal_abc.csv")
-write_csv(journal_count, file = "./data/2021_docuserve_doc_del_journal_count.csv")
+write_csv(journal_abc, file = "./data/2023_docuserve_doc_del_journal_abc.csv")
+write_csv(journal_count, file = "./data/2023_docuserve_doc_del_journal_count.csv")
 
 
